55cf0f2670b576ecd0966170b77414886ac0dc6f
HenriqueReinaldi <henrique.f.reinaldi@gmail.com>
2026-08-14 18:17:48 -0300

    feat: criar classe permanencia

---
+++ src/main/java/org/henrique/terminal/Terminal.java
@@ -0,0 +1,15 @@
+package org.henrique.terminal;
+
+import org.henrique.permanencia.Permanencia;
+
+public class Terminal {
+    private Permanencia perm;
+
+    public void iniciar(){
+
+    }
+
+    public Terminal(Permanencia perm){
+        this.perm = perm;
+    }
+}
