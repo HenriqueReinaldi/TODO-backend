07d951582227f2402d7fb37244250bb87c7e6741
HenriqueReinaldi <henrique.f.reinaldi@gmail.com>
2026-08-14 19:52:42 -0300

    feat: operacoes CRUD com tarefas terminal

--- src/main/java/org/henrique/terminal/Terminal.java
+++ src/main/java/org/henrique/terminal/Terminal.java
@@ -6,7 +6,10 @@ import org.henrique.permanencia.Permanencia;
 
 
 import java.lang.reflect.Array;
+import java.sql.Time;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Terminal {
@@ -14,7 +17,7 @@ public class Terminal {
     private Quadro_de_tarefas quadro_carregado;
 
     private modo_term mode = modo_term.SEM_QUADRO;
-    private modo_listagem mode_list = modo_listagem.L_PRIORIDADE;
+    private modo_listagem mode_list = modo_listagem.L_STATUS;
     private int mostar_prox = 1;
 
     private void interpretar_modoz(String in){
@@ -68,12 +71,83 @@ public class Terminal {
         }
 
         if (in.startsWith("nova ") && in.length() > 5){
+            String nome = in.substring(5);
+            if (quadro_contem(nome)) return;
+
+            Tarefa nova = new Tarefa(in.substring(5), "", "", 0, "", "TODO");
+            quadro_carregado.append_tarefa(nova);
             return;
         }
 
         if (in.startsWith("ver ") && in.length() > 4){
-
+            ver_tarefa(in.substring(4));
             mostar_prox = 0;
+            return;
+        }
+
+        if (in.startsWith("todo ") && in.length() > 5){
+            String nome = in.substring(5);
+            Tarefa t = get_ref_quadro(nome);
+            if (t == null) return;
+
+            t.status = "TODO";
+            return;
+        }
+        if (in.startsWith("doing ") && in.length() > 6){
+            String nome = in.substring(6);
+            Tarefa t = get_ref_quadro(nome);
+            if (t == null) return;
+
+            t.status = "DOING";
+            return;
+        }
+        if (in.startsWith("done ") && in.length() > 5){
+            String nome = in.substring(5);
+            Tarefa t = get_ref_quadro(nome);
+            if (t == null) return;
+
+            t.status = "DONE";
+            t.data_termino = LocalDateTime.now().toString();
+            return;
+        }
+
+
+        if (in.startsWith("prioridade ") && in.length() > 11){
+            String[] args = in.substring(11).split(" ", 2);
+            if (args.length != 2) return;
+
+            Tarefa t = get_ref_quadro(args[0]);
+            if (t == null) return;
+
+            t.prioridade = Integer.parseInt(args[1]);
+
+        }
+        if (in.startsWith("descricao ") && in.length() > 10){
+            String[] args = in.substring(10).split(" ", 2);
+            if (args.length != 2) return;
+
+            Tarefa t = get_ref_quadro(args[0]);
+            if (t == null) return;
+
+            t.desc = args[1];
+        }
+        if (in.startsWith("categoria ") && in.length() > 10){
+            String[] args = in.substring(10).split(" ");
+            if (args.length < 2) return;
+
+            Tarefa t = get_ref_quadro(args[0]);
+            if (t == null) return;
+
+            t.categoria = args[1];
+        }
+
+        if (in.startsWith("del ") && in.length() > 4) {
+            String nome = in.substring(4);
+
+            Tarefa ref = get_ref_quadro(nome);
+            if (ref == null) return;
+
+            quadro_carregado.lista_de_tarefas.remove(ref);
             return;
         }
 
@@ -84,6 +158,35 @@ public class Terminal {
     }
 
 
+    private boolean quadro_contem(String nome){
+        for (Tarefa t : quadro_carregado.lista_de_tarefas){
+            if (t.nome.equals(nome)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Tarefa get_ref_quadro(String nome){
+        for (Tarefa t : quadro_carregado.lista_de_tarefas){
+            if (t.nome.equals(nome)){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    private void ver_tarefa(String nome){
+        Tarefa ref = get_ref_quadro(nome);
+        if (ref == null) return;
+
+        System.out.printf("%s:==================\n\n", ref.nome);
+
+        System.out.println("    descrição:" + ref.desc);
+        System.out.println("    termino  :" + ref.data_termino);
+
+        System.out.println("\n==========================\n");
+    }
     private void listar_por_categoria(){
         ArrayList<String> prioridades = new ArrayList<>();
 
@@ -134,6 +237,8 @@ public class Terminal {
             prioridades.add(t.prioridade);
         }
 
+        Collections.sort(prioridades);
+
         for (Integer pri : prioridades){
             System.out.printf("  %-4d:===================\n\n", pri);
 
@@ -169,7 +274,7 @@ public class Terminal {
                 break;
         }
 
-        System.out.println("==========================");
+        System.out.println("==========================\n");
     }
 
     public void iniciar(){
@@ -191,7 +296,8 @@ public class Terminal {
                 listar_tarefas();
 
                 System.out.print("@"+ quadro_carregado.nome +">");
-                interpretar_modoo(scan.nextLine());
+                String comando = scan.nextLine();
+                interpretar_modoo(comando);
             }
             else {
                 break;
